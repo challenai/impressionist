15922b31315d56fcb07a433439c5acbffaa1e908
ChallenAi <fengyiai@gmail.com>
2024-12-29 09:56:13 +0000

    feat: add max radius limit, add aligned shapes

--- lib/rectangle.ts
+++ lib/rectangle.ts
@@ -26,11 +26,17 @@ export class Rectangle {
     if (typeof radius === "number") {
       radius = { tl: radius, tr: radius, br: radius, bl: radius };
     }
+    const maxLen = Math.min(width, height) / 2;
 
     if (!radius.tl) radius.tl = 0;
     if (!radius.tr) radius.tr = 0;
     if (!radius.bl) radius.bl = 0;
     if (!radius.br) radius.br = 0;
+
+    radius.tl = Math.min(radius.tl, maxLen);
+    radius.tr = Math.min(radius.tr, maxLen);
+    radius.bl = Math.min(radius.bl, maxLen);
+    radius.br = Math.min(radius.br, maxLen);
 
     return `
       M${x - (width - radius.tl - radius.tr) / 2} ${y - height / 2}
@@ -68,5 +74,49 @@ export class Rectangle {
       l${-width} 0
       Z
     `;
+  }
+
+  static BasicAligned(
+    x: number,
+    y: number,
+    width: number,
+    height: number,
+  ): string {
+    return this.Basic(x + width / 2, y + height / 2, width, height);
+  }
+
+  static RoundAligned(
+    x: number,
+    y: number,
+    width: number,
+    height: number,
+    radius: number | Radius4
+  ): string {
+    return this.Round(x + width / 2, y + height / 2, width, height, radius);
+  }
+
+  static DiamondAlignedTop(
+    x: number,
+    y: number,
+    width: number,
+    height: number,
+  ): string {
+    return this.Diamond(x, y + height / 2, width, height);
+  }
+
+  static DiamondAlignedLeft(
+    x: number,
+    y: number,
+    width: number,
+    height: number,
+  ): string {
+    return this.Diamond(x + width / 2, y, width, height);
+  }
+
+  static ParallelogramAligned(x: number, y: number, width: number, height: number, offset: number): string {
+    if (offset < 0) {
+      return this.Parallelogram(x + width / 2 - offset / 2, y + height / 2, width, height, offset);
+    }
+    return this.Parallelogram(x + width / 2 + offset / 2, y + height / 2, width, height, offset);
   }
 }
