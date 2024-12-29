0f6a87c2286009cdca5e32d841ce8b30b1ebf228
ChallenAi <fengyiai@gmail.com>
2024-12-29 09:56:36 +0000

    feat: add aligned triangles

--- lib/triangle.ts
+++ lib/triangle.ts
@@ -19,4 +19,67 @@ export class Triangle {
       Z
     `;
   }
+
+  static EquilateralAlignedLeft(x: number, y: number, length: number): string {
+    const h = Math.sin(Math.PI / 3) * length;
+    const mx = length / 2;
+    return `
+      M${x} ${y}
+      l${length} 0
+      l${-mx} ${-h}
+      Z
+    `;
+  }
+
+  static EquilateralAlignedTop(x: number, y: number, length: number): string {
+    const h = Math.sin(Math.PI / 3) * length;
+    const mx = length / 2;
+    return `
+      M${x - mx} ${y + h}
+      l${length} 0
+      l${-mx} ${-h}
+      Z
+    `;
+  }
+
+  static EquilateralAlignedRight(x: number, y: number, length: number): string {
+    const h = Math.sin(Math.PI / 3) * length;
+    const mx = length / 2;
+    return `
+      M${x - length} ${y}
+      l${length} 0
+      l${-mx} ${-h}
+      Z
+    `;
+  }
+
+  static IsoscelesAlignedLeft(x: number, y: number, bottom: number, height: number): string {
+    const mx = bottom / 2;
+    return `
+      M${x} ${y}
+      l${bottom} 0
+      l${-mx} ${-height}
+      Z
+    `;
+  }
+
+  static IsoscelesAlignedTop(x: number, y: number, bottom: number, height: number): string {
+    const mx = bottom / 2;
+    return `
+      M${x - mx} ${y + height}
+      l${bottom} 0
+      l${-mx} ${-height}
+      Z
+    `;
+  }
+
+  static IsoscelesAlignedRight(x: number, y: number, bottom: number, height: number): string {
+    const mx = bottom / 2;
+    return `
+      M${x - bottom} ${y}
+      l${bottom} 0
+      l${-mx} ${-height}
+      Z
+    `;
+  }
 }
